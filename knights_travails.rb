03992af021786aef4fc649477ac12048b6d2f84e
Eliott Ham <eliotthham@gmail.com>
2017-01-26 00:23:55 -0800

    Created #knight_moves method to run the script.

--- knights_travails.rb
+++ knights_travails.rb
@@ -17,10 +17,10 @@ class PositionTree
 		@path = []
 	end
 
-	def backwards_search
-		queue = [forwards_search]
-		until queue.empty?
-			return if queue.first.position == @initial
+	def backwards_search																	#Run a BFS from the found target position
+		queue = [forwards_search]															#to find the shortest path of the paths
+		until queue.empty?																	#that found the target by checking
+			return if queue.first.position == @initialize									#each node's parent
 			queue.first.parent.each do |parent|
 				queue << parent
 			end
@@ -29,9 +29,9 @@ class PositionTree
 		end
 	end
 
-	def forwards_search
-		queue = [@initial]
-		until queue.empty?
+	def forwards_search																		#Run a BFS from the initial position
+		queue = [@initial]																	#to find the target position by checking
+		until queue.empty?																	#through each node's children
 			return queue.first if queue.first.position == @target
 			find_children(queue.first)
 			queue.first.children.each do |child|
@@ -46,32 +46,27 @@ class PositionTree
 		offset = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
 
 		offset.each do |x|																	#Adds the offsets from the current position
-			new_position = [x[0] + node.row, x[1] + node.col]								#and checks to see if they fit on the board
-			if (0..7).include?(new_position[0]) and (0..7).include?(new_position[1])
-				child = Position.new(new_position)
-				node.children << child
+			new_position = [x[0] + node.row, x[1] + node.col]								#and checks to see if they fit on the board.
+			if (0..7).include?(new_position[0]) and (0..7).include?(new_position[1])		#Positions that fit are made into nodes and
+				child = Position.new(new_position)											#are added to the current positions array
+				node.children << child  													#of children.
 				child.parent << node
 			end
 		end
 	end
 
-	def path
-		forwards_search
-		backwards_search
+	def find_path																			#Runs both search methods to find
+		forwards_search																		#the shortest possible path and
+		backwards_search																	#returns the path as an array of Position nodes.
 		return @path		
 	end
 end
 
-
-
-
-
-
-
-
-pt = PositionTree.new([3,3], [4,3])
-puts pt.path
-
-
-
-
+def knight_moves(initial, target)
+	moves = PositionTree.new(initial, target)
+	path = moves.find_path
+	puts "You made it in #{path.length} moves! Here's your path:"
+	until path.empty?
+		print "#{path.pop.position} \n"
+	end
+end

dfd77f7d39d26202c3b4fcd8d3c21de2bca7cff7
Eliott Ham <eliotthham@gmail.com>
2017-01-26 00:08:14 -0800

    Changed name #find_positions to #find_children. Created #forwards_serach and #backwards_search to find shortest path.

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,44 +1,77 @@
 class Position
-	attr_accessor :position, :potential, :previous
+	attr_accessor :position, :children, :previous, :parent
 	attr_reader :row, :col
-	def initialize(position, parent=nil)
+	def initialize(position)
 		@position = position
-		@potential = Array.new
 		@row = position[0]
 		@col = position[1]
-		@previous = parent
+		@children = Array.new
+		@parent = Array.new
 	end
 end
 
-def find_positions(position_instance)
-	offset = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
-	current = position_instance
+class PositionTree
+	def initialize(initial, target)
+		@initial = Position.new(initial)
+		@target = target
+		@path = []
+	end
 
-	offset.each do |x|
-		new_position = [x[0] + current.row, x[1] + current.col]
-		if (0..7).include?(new_position[0]) and (0..7).include?(new_position[1])
-			current.potential << Position.new(new_position, current)
+	def backwards_search
+		queue = [forwards_search]
+		until queue.empty?
+			return if queue.first.position == @initial
+			queue.first.parent.each do |parent|
+				queue << parent
+			end
+			@path << queue.first
+			queue.shift
 		end
 	end
-	check_positions(current.potential)
+
+	def forwards_search
+		queue = [@initial]
+		until queue.empty?
+			return queue.first if queue.first.position == @target
+			find_children(queue.first)
+			queue.first.children.each do |child|
+				queue << child
+			end
+			queue.shift
+		end
+		return nil
+	end
+
+	def find_children(node)
+		offset = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
+
+		offset.each do |x|																	#Adds the offsets from the current position
+			new_position = [x[0] + node.row, x[1] + node.col]								#and checks to see if they fit on the board
+			if (0..7).include?(new_position[0]) and (0..7).include?(new_position[1])
+				child = Position.new(new_position)
+				node.children << child
+				child.parent << node
+			end
+		end
+	end
+
+	def path
+		forwards_search
+		backwards_search
+		return @path		
+	end
 end
 
-def check_positions(potential_array)
 
 
 
 
 
 
-	
+
+pt = PositionTree.new([3,3], [4,3])
+puts pt.path
 
 
 
-
-def knight_moves(initial, target)
-	initial = Position.new(initial)
-	find_positions(initial)
-end
-
-knight_moves([0, 0], 0)
 
